03ad6e604f81148c2727a73725de3bc51343e5b5
unjinjang <alpsboy1996@gmail.com>
2022-06-05 03:58:32 +0900

    [FEAT]: Add basic React router feature

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
+
+import GlobalStyle from "./styles/index";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <React.StrictMode>
+      <BrowserRouter>
+        <GlobalStyle />
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </>
 );
