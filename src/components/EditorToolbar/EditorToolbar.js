3b179f01b14b19252e588874ad1118589db45116
unjinjang <alpsboy1996@gmail.com>
2022-06-06 01:04:10 +0900

    [FEAT]: Add static version EditorToolbar component

---
+++ src/components/EditorToolbar/EditorToolbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+function EditorToolbar({ children }) {
+  return <Toolbar></Toolbar>;
+}
+
+const Toolbar = styled.div`
+  background-color: rgb(249, 250, 251);
+  height: 84vh;
+  width: 15.5%;
+`;
+
+export default EditorToolbar;
