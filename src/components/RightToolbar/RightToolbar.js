25dfe90af397689e820ffa0c2480a0949065e53b
rami <aarmakrr@hanmail.net>
2022-06-07 01:58:03 +0900

    [FEAT]: Add button editor

--- src/components/RightToolbar/RightToolbar.js
+++ src/components/RightToolbar/RightToolbar.js
@@ -1,19 +1,34 @@
 import React from "react";
 
+import ToolbarButton from "../Button/ToolbarButton";
 import EditorToolbar from "../EditorToolbar/EditorToolbar";
+import ButtonEditor from "../SubToolbar/ButtonEditor";
+import ButtonOpacityEditor from "../SubToolbar/ButtonOpacityEditor";
+import ButtonRadiusEditor from "../SubToolbar/ButtonRadiusEditor";
 import CanvasClearButton from "../SubToolbar/CanvasClearButton";
-import TextColorEditor from "../SubToolbar/TextColorEditor";
+import ColorEditor from "../SubToolbar/ColorEditor";
 import TextDetailEditor from "../SubToolbar/TextDetailEditor";
 import TextPositionEditor from "../SubToolbar/TextPositionEditor";
 import ToolbarContainer from "../ToolbarContainer/ToolbarContainer";
 
-function RightToolbar() {
+function RightToolbar({ name }) {
   return (
     <EditorToolbar>
       <ToolbarContainer>
-        <TextDetailEditor />
-        <TextPositionEditor title={"edit positon"} />
-        <TextColorEditor />
+        {name === "text" && (
+          <>
+            <TextDetailEditor title={"Edit text"} />
+            <TextPositionEditor title={"Edit positon"} />
+          </>
+        )}
+        {name === "button" && (
+          <>
+            <ButtonEditor />
+            <ButtonRadiusEditor />
+            <ButtonOpacityEditor />
+          </>
+        )}
+        <ColorEditor />
         <CanvasClearButton />
       </ToolbarContainer>
     </EditorToolbar>
