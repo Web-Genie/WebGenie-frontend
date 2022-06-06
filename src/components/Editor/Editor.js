6508f49d147fdb729669e073f5a9129cc2163d41
unjinjang <alpsboy1996@gmail.com>
2022-06-06 19:27:52 +0900

    [FEAT]: Add useState inside Editor for more flexible title changing feature + buttons to display modal with informations based on a specific button

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaArrowLeft, FaRegEdit } from "react-icons/fa";
 import styled from "styled-components";
 
@@ -8,11 +8,53 @@ import EditorTemplate from "../EditorTemplate/EditorTemplate";
 import EditorToolbar from "../EditorToolbar/EditorToolbar";
 import Header from "../Header/Header";
 import LeftToolbar from "../LeftToolbar/LeftToolbar";
+import Modal from "../Modal/Modal";
+import ModalContent from "../ModalContent/ModalContent";
 import Navigation from "../Navigation/Navigation";
 
 function Editor() {
+  const [shouldDisplayModal, setShouldDisplayModal] = useState(false);
+  const [messageContent, setMessageContent] = useState("");
+  const [buttonText, setButtonText] = useState("");
+  const [modalIconState, setModalIconState] = useState("");
+  const [shouldEditTitle, setShouldEditTitle] = useState(false);
+  const [editorTitle, setEditorTitle] = useState("New Title 2");
+
+  const toggleSaveModal = () => {
+    setShouldDisplayModal(!shouldDisplayModal);
+    setMessageContent("현재 웹사이트를 저장하시겠습니까?");
+    setButtonText("네, 저장할게요");
+    setModalIconState("save");
+  };
+
+  const togglePublishModal = () => {
+    setShouldDisplayModal(!shouldDisplayModal);
+    setMessageContent("현재 웹사이트를 배포하시겠습니까?");
+    setButtonText("네, 배포할게요");
+    setModalIconState("deploy");
+  };
+
+  const handleTitleChangeState = () => {
+    setShouldEditTitle(!shouldEditTitle);
+  };
+
+  const listenTitleInputChange = (ev) => {
+    setEditorTitle(ev.target.value);
+  };
+
   return (
     <>
+      {shouldDisplayModal && (
+        <Modal>
+          <ModalContent
+            modalText={messageContent}
+            primaryButtonText={buttonText}
+            secondaryButtonText={"아니요"}
+            modalIconState={modalIconState}
+            handleClick={toggleSaveModal}
+          />
+        </Modal>
+      )}
       <Header>
         <h1>WebGenie</h1>
         <img src={mockImage} />
@@ -23,15 +65,22 @@ function Editor() {
             <FaArrowLeft />
           </span>
           <div className="titleNavbar">
-            <h3>New Editor 2</h3>
-            <span>
+            {!shouldEditTitle && <h3>{editorTitle}</h3>}
+            {shouldEditTitle && (
+              <input onChange={listenTitleInputChange}></input>
+            )}
+            <span onClick={handleTitleChangeState}>
               <FaRegEdit />
             </span>
           </div>
         </div>
         <div>
-          <Button mainButton={false}>Save</Button>
-          <Button mainButton={true}>Publish</Button>
+          <Button handleClick={toggleSaveModal} mainButton={false}>
+            Save
+          </Button>
+          <Button handleClick={togglePublishModal} mainButton={true}>
+            Publish
+          </Button>
         </div>
       </Navigation>
       <EditorBody>
