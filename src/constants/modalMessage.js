1d186dcc88f73fe144e6c77a0c8ad51e60285b56
unjinjang <alpsboy1996@gmail.com>
2022-06-06 21:18:02 +0900

    [CHORE]: File rename from modalMessages.js -> constants.js

--- src/constants/modalMessage.js
+++
@@ -1,9 +0,0 @@
-export const NEW_EDITOR_REMINDER_MESSAGE =
-  "새로운 웹사이트 에디터를 생성할까요?";
-export const NEW_EDITOR_ACCEPT_MESSAGE = "네, 만들어주세요.";
-
-export const SAVE_REMINDER_MESSAGE = "현재 웹사이트를 저장하시겠습니까?";
-export const SAVE_ACCEPT_MESSAGE = "네, 저장할게요.";
-
-export const PUBLISH_REMINDER_MESSAGE = "현재 웹사이트를 배포하시겠습니까?";
-export const PUBLISH_ACCEPT_MESSAGE = "네, 저장할게요.";
