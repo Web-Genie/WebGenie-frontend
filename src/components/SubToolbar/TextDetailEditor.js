7d0d94856c1af36c8bb159a3b8bac60f62a9d5aa
rami0617 <aarmakrr@hanmail.net>
2022-06-07 14:12:55 +0900

    [FEAT]: Add seperate select option

--- src/components/SubToolbar/TextDetailEditor.js
+++ src/components/SubToolbar/TextDetailEditor.js
@@ -2,23 +2,24 @@ import React from "react";
 import { FaBold, FaItalic, FaUnderline } from "react-icons/fa";
 
 import ToolbarButton from "../Button/ToolbarButton";
+import SelectDetail from "./SelectDetail";
 import SubtoolbarTitle from "./SubtoolbarTitle";
 
 function TextDetailEditor() {
+  const fontType = ["inter", "고딕", "굴림"];
+  const fontSize = Array(50)
+    .fill(1)
+    .map((n, i) => n + i);
+
   return (
     <div className="choiceContainer">
       <SubtoolbarTitle title={"Edit text"} />
       <div className="textDetailChoice">
         <ToolbarButton>
-          <select className="fontType">
-            <option>inter</option>
-            <option>고딕</option>
-          </select>
+          <SelectDetail detailData={fontType} className={"fontType"} />
         </ToolbarButton>
         <ToolbarButton>
-          <select className="fontSize">
-            <option>24</option>
-          </select>
+          <SelectDetail detailData={fontSize} className={"fontSize"} />
         </ToolbarButton>
       </div>
       <div className="detailContainer">
