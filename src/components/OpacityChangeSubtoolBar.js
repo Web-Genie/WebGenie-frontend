8a71887bf7b50725eed5de804c3bc9e4a8450c56
inbeen <blueline1318@gmail.com>
2022-06-08 15:52:09 +0900

    [FEAT]: Add image opacity func

--- src/components/OpacityChangeSubtoolBar.js
+++ src/components/OpacityChangeSubtoolBar.js
@@ -4,11 +4,11 @@ import { OPACITY_RANGE } from "../constants/constants";
 import SelectDetail from "./SelectDetailSubtoolBar";
 import SubtoolbarTitle from "./SubtoolbarTitle";
 
-function OpacityEditorSubtoolBar() {
+function OpacityEditorSubtoolBar({ handleImgOpacity }) {
   return (
     <div className="choiceContainer">
       <SubtoolbarTitle title="Edit Opacity" />
-      <SelectDetail title="opacity" detailValue={OPACITY_RANGE} />
+      <SelectDetail title="opacity" detailValue={OPACITY_RANGE} handleImgOpacity={handleImgOpacity}/>
     </div>
   );
 }
