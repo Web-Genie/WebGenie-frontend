018c35f3100eb4831c532a0ebd2e20af4e36a336
rami <aarmakrr@hanmail.net>
2022-06-10 02:01:49 +0900

    [REFACTOR]: Refactoring overall change color logic

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -31,7 +31,6 @@ function Editor() {
   };
 
   const [color, setColor] = useState("");
-
   const [imageOpacity, setImageOpacity] = useState(1);
   const [imageBrightness, setImageBrightness] = useState(1);
   const [imageBlur, setImageBlur] = useState(0);
@@ -95,12 +94,16 @@ function Editor() {
       </Navigation>
       <EditorBody>
         {!shouldShowWideView && <LeftToolbar />}
-        <EditorTemplate displayWideView={shouldShowWideView} />
+        <EditorTemplate
+          displayWideView={shouldShowWideView}
+          colorName={color}
+        />
         {!shouldShowWideView && (
           <RightToolbar
             onChangeOpacity={handleImgOpacity}
             onChangeBrightness={handleImgBrightness}
             onChangeBlur={handleImgBlur}
+            changeColor={setColor}
           />
         )}
       </EditorBody>
