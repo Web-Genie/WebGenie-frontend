ba20f36bb44e0176579b0b0b494c04dced443c6d
unjinjang <alpsboy1996@gmail.com>
2022-06-11 07:53:05 +0900

    [REFACTOR]: Remove useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Editor from "./components/Editor";
