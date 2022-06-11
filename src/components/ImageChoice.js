f7bbba7072a39ce7328839327df49fa6c23bc867
rami0617 <aarmakrr@hanmail.net>
2022-06-11 15:52:29 +0900

    [FEAT]: Add local image upload

--- src/components/ImageChoice.js
+++ src/components/ImageChoice.js
@@ -1,8 +1,29 @@
 import { FaCloudUploadAlt, FaImage } from "react-icons/fa";
+import { MdViewInAr } from "react-icons/md";
 
 import ToolbarButton from "./ToolbarButton";
 
 function ImageChoice() {
+  const handleImage = (event) => {
+    const file = event.target.files[0];
+    // console.log();
+    // const name = document.querySelector(".sc-gsnTZi iGRmMq");
+    // console.log(name);
+    // name.textContent = file.name;
+
+    const imageDiv = document.createElement("div");
+    const newImage = document.createElement("img");
+    newImage.setAttribute("class", "img");
+
+    newImage.src = URL.createObjectURL(file);
+    console.log(newImage);
+    imageDiv.appendChild(newImage);
+    // const container = document.body.
+    // console.log(container);
+    document.body
+      .getElementsByClassName("sc-gsnTZi iGRmMq")[0]
+      .appendChild(imageDiv);
+  };
   return (
     <div className="choiceContainer">
       <div className="category">
@@ -11,7 +32,9 @@ function ImageChoice() {
       <div className="imageUploadingChoice">
         <ToolbarButton>
           <FaCloudUploadAlt />
-          <p className="text">Upload</p>
+          <input type="file" className="text" onChange={handleImage}>
+            {/* Upload */}
+          </input>
         </ToolbarButton>
         <ToolbarButton>
           <FaImage />
