08c5e68a68bc281d0259a95e865de3377bdd46ce
unjinjang <alpsboy1996@gmail.com>
2022-06-11 18:40:12 +0900

    [REFACTOR]: Based on code review

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -43,7 +43,6 @@ function Editor() {
     closeModal,
     message,
   } = useModal(userTitle, currentEditorId);
-
   const toggleWideView = () => {
     setShouldShowWideView((state) => !state);
   };
