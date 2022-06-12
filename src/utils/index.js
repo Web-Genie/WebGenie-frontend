1de58b7ee06f049461d8e8ce0d11fc64fb4d1dd4
unjinjang <alpsboy1996@gmail.com>
2022-06-13 01:31:43 +0900

    [FEAT]: Add utility function to create a new node for deletion and changing text

--- src/utils/index.js
+++ src/utils/index.js
@@ -76,26 +76,62 @@ export const sendUserToHomepage = () => {
   window.location.replace("/");
 };
 
-export const generateEditorDeleteElement = () => {
-  const deleteNodeElement = document.createElement("p");
+export const deleteCurrentElement = (event) => {
+  event.stopPropagation();
+  event.target.nextSibling.remove();
+  event.target.nextElementSibling.remove();
+};
+
+export const generateEditorDeleteElement = (
+  clickedNode,
+  icon,
+  editIcon = false
+) => {
+  const extraIconElement = document.createElement("p");
 
   const deleteCurrentElement = (event) => {
     event.stopPropagation();
-    event.target.parentNode.remove();
+    event.target.nextSibling.remove();
+    event.target.nextElementSibling.remove();
+    event.target.remove();
   };
 
-  deleteNodeElement.style.display = "flex";
-  deleteNodeElement.style.justifyContent = "center";
-  deleteNodeElement.style.alignItems = "center";
-  deleteNodeElement.style.position = "absolute";
-  deleteNodeElement.innerHTML = "&#x2715;";
-  deleteNodeElement.style.background = "rgba(0,0,0,0.4)";
-  deleteNodeElement.style.color = "white";
-  deleteNodeElement.style.borderRadius = "50%";
-  deleteNodeElement.style.width = "30px";
-  deleteNodeElement.style.height = "30px";
-  deleteNodeElement.style.fontSize = "23px";
-  deleteNodeElement.onclick = deleteCurrentElement;
+  const extraIconMouseOverEffect = (event) => {
+    event.target.style.transform = "scale(1.1)";
+  };
 
-  return deleteNodeElement;
+  const extraIconMouseLeaveEffect = (event) => {
+    event.target.style.transform = "scale(1)";
+  };
+
+  extraIconElement.style.display = "flex";
+  extraIconElement.style.justifyContent = "center";
+  extraIconElement.style.alignItems = "center";
+  extraIconElement.style.position = "absolute";
+  extraIconElement.innerHTML = icon;
+  extraIconElement.style.background = "rgba(0,0,0,0.7)";
+  extraIconElement.style.color = "white";
+  extraIconElement.style.borderRadius = "50%";
+  extraIconElement.style.width = "30px";
+  extraIconElement.style.height = "30px";
+  extraIconElement.style.cursor = "pointer";
+  extraIconElement.style.transition = "all 0.15s ease";
+  extraIconElement.style.zIndex = "100";
+
+  if (editIcon) {
+    extraIconElement.style.left = `${clickedNode.offsetLeft + 25}px`;
+    extraIconElement.style.top = `${clickedNode.offsetTop - 20}px`;
+    extraIconElement.onmouseleave = extraIconMouseLeaveEffect;
+    extraIconElement.onmouseover = extraIconMouseOverEffect;
+  } else {
+    extraIconElement.style.left = `${clickedNode.offsetLeft - 15}px`;
+    extraIconElement.style.top = `${clickedNode.offsetTop - 20}px`;
+    extraIconElement.onmouseleave = extraIconMouseLeaveEffect;
+    extraIconElement.onmouseover = extraIconMouseOverEffect;
+    extraIconElement.onclick = deleteCurrentElement;
+  }
+
+  extraIconElement.style.fontSize = "23px";
+
+  return extraIconElement;
 };
