525b6953fdb92a42ab7f3eaa26b3255cd8491689
unjinjang <alpsboy1996@gmail.com>
2022-06-12 22:17:31 +0900

    [REFACTOR]: Remove unused logic + console.log statement

--- src/components/ModalContent.js
+++ src/components/ModalContent.js
@@ -55,7 +55,7 @@ function ModalContent({
         </NewSiteModalInputContainer>
       )}
       <Button
-        handleClick={goHomepage ? sendUserToHomepage : fetchData}
+        handleClick={shouldGoHomepage ? sendUserToHomepage : fetchData}
         mainButton={true}
       >
         {primaryButtonText}
