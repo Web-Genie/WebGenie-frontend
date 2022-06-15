9ec822dd0b0b7a35e1bf296f0c3bb9c5ecc0407a
unjinjang <alpsboy1996@gmail.com>
2022-06-15 14:56:38 +0900

    [REFACTOR]: Change code displaying order

--- src/components/DeployedWebsite.js
+++ src/components/DeployedWebsite.js
@@ -1,11 +1,10 @@
 import { useContext, useEffect, useRef } from "react";
-import styled from "styled-components";
 
 import { UserContext } from "../context/userContext";
 import useAxios from "../hooks/useAxios";
 
 function DeployedWebsite({ children }) {
-  const { editor } = useContext(UserContext);
+  const { editor, setEditor } = useContext(UserContext);
   const mainpageRef = useRef(null);
   const deployedEditorURL = window.location.pathname.split("/")[2];
   const { fetchData } = useAxios({
@@ -22,9 +21,11 @@ function DeployedWebsite({ children }) {
 
   useEffect(() => {
     if (editor) {
-      mainpageRef.current.innerHTML = editor.result.userSavedCode;
-      console.log(editor);
+      const savedCodeCollection = editor.result.userSavedCode;
+      mainpageRef.current.innerHTML = savedCodeCollection[0].code;
     }
+
+    setEditor(null);
   }, [editor]);
 
   return <div ref={mainpageRef}>{children}</div>;
