c0238be90831e07c31d110cbc9325d54d7269ac5
rami0617 <aarmakrr@hanmail.net>
2022-06-16 21:25:03 +0900

    [REFACTOR]: Fix no modal if nothing changes

--- src/services/setupProxy.js
+++ src/services/setupProxy.js
@@ -3,7 +3,7 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/", {
-      target: process.env.API_ENDPOINT_URL || "http://localhost:8080",
+      target: "http://webgenie.click/",
       changeOrigin: true,
     })
   );
