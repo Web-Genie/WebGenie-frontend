a7608597c558119488f246ce04e515f665547296
unjinjang <alpsboy1996@gmail.com>
2022-06-20 21:26:52 +0900

    [REFACTOR]: Handle Conflict

--- src/components/UserCollection.test.js
+++ src/components/UserCollection.test.js
@@ -5,20 +5,16 @@ import UserCollection from "../components/UserCollection";
 import { SubToolbarTypeProvider } from "../context/subToolbarContext";
 import { UserContextTypeProvider } from "../context/userContext";
 
-describe("", () => {
-  test("1. 사용자가 생성한 사이트가 아무것도 없을 때 '생성된 웹 사이트가 없습니다'라고 나타나야한다.", () => {
-    render(
-      <MemoryRouter>
-        <SubToolbarTypeProvider>
-          <UserContextTypeProvider>
-            <UserCollection />
-          </UserContextTypeProvider>
-        </SubToolbarTypeProvider>
-      </MemoryRouter>
-    );
+test("1. 사용자가 생성한 사이트가 아무것도 없을 때 '생성된 웹 사이트가 없습니다'라고 나타나야한다.", () => {
+  render(
+    <MemoryRouter>
+      <SubToolbarTypeProvider>
+        <UserContextTypeProvider>
+          <UserCollection />
+        </UserContextTypeProvider>
+      </SubToolbarTypeProvider>
+    </MemoryRouter>
+  );
 
-    expect(screen.getByText("생성된 웹사이트가 없습니다.")).toBeInTheDocument();
-  });
-
-  screen.debug();
+  expect(screen.getByText("생성된 웹사이트가 없습니다.")).toBeInTheDocument();
 });
