1d191b43aac2b9b7283b10811e99c0fef46fd76d
unjinjang <alpsboy1996@gmail.com>
2022-06-20 21:46:13 +0900

    [FEAT]: Add test code for ButtonChoice.js

---
+++ src/components/ButtonChoice.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+
+import ButtonChoice from "./ButtonChoice";
+
+it("Displays button choice header and button correctly", () => {
+  render(<ButtonChoice />);
+
+  expect(screen.getByText("Add Button")).toBeInTheDocument();
+  expect(screen.getByText("Button")).toBeInTheDocument();
+});
