5bd0db3bc585f9996fd9dd6ece42d0bf16c04ffa
blueline1318 <blueline1318@gmail.com>
2022-06-21 17:42:19 +0900

    [FEAT]: Add test code for UserCollection.js

--- src/components/UserCollection.test.js
+++ src/components/UserCollection.test.js
@@ -5,7 +5,21 @@ import UserCollection from "../components/UserCollection";
 import { SubToolbarTypeProvider } from "../context/subToolbarContext";
 import { UserContextTypeProvider } from "../context/userContext";
 
-describe("", () => {
+describe("<UserCollection />", () => {
+  const searchKeyword = [];
+  const sampleCollections = [
+    {
+      _id: "1",
+      author: "test1",
+      title: "test1",
+    },
+    {
+      _id: "2",
+      author: "test2",
+      title: "test2",
+    },
+  ];
+
   test("1. 사용자가 생성한 사이트가 아무것도 없을 때 '생성된 웹 사이트가 없습니다'라고 나타나야한다.", () => {
     render(
       <MemoryRouter>
@@ -20,5 +34,21 @@ describe("", () => {
     expect(screen.getByText("생성된 웹사이트가 없습니다.")).toBeInTheDocument();
   });
 
-  screen.debug();
+  test("2. 사용자가 생성한 사이트가 있다면, 해당 사이트를 모두 보여주어야 한다.", () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <SubToolbarTypeProvider>
+          <UserContextTypeProvider>
+            <UserCollection
+              collections={sampleCollections}
+              searchKeyword={searchKeyword}
+            />
+          </UserContextTypeProvider>
+        </SubToolbarTypeProvider>
+      </MemoryRouter>
+    );
+
+    expect(getByText(sampleCollections[0].title)).toHaveTextContent("test1");
+    expect(getByText(sampleCollections[1].title)).toHaveTextContent("test2");
+  });
 });
