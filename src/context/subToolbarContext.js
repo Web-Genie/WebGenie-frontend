c92d7d862c310b04e62c9cfb181a45f567fbb5a4
unjinjang <alpsboy1996@gmail.com>
2022-07-30 07:44:26 +0900

    [REFACTOR]: Removed poor context api logic

--- src/context/subToolbarContext.js
+++ src/context/subToolbarContext.js
@@ -6,21 +6,11 @@ const SubToolbarContext = createContext(undefined);
 const InputFieldContext = createContext(undefined);
 
 const SubToolbarTypeProvider = ({ children }) => {
-  const [subToolbarType, setSubToolbarType] = useState();
-  const [isBold, setIsBold] = useState(false);
-  const [isItalic, setIsItalic] = useState(false);
-  const [isUnderLine, setIsUnderLine] = useState(false);
-  const [textAlign, setTextAlign] = useState("");
-  const [colorValue, setColorValue] = useState("");
-  const [buttonColor, setButtonColor] = useState("");
   const [localImageSrc, setLocalImageSrc] = useState("");
   const [imageUrl, setImageUrl] = useState("");
   const [hasImageUrl, setHasImageUrl] = useState(false);
   const [clearCanvasBackgroundColor, setClearCanavasBackgroundColor] =
     useState("");
-  const [isCanvasClear, setIsCanvasClear] = useState(false);
-  const [imageBrightness, setImageBrightness] = useState(1);
-  const [imageBlur, setImageBlur] = useState(0);
 
   const {
     inputValue,
@@ -34,45 +24,19 @@ const SubToolbarTypeProvider = ({ children }) => {
     setButtonRadius,
     buttonOpacity,
     setButtonOpacity,
-    fontSize,
-    setFontSize,
-    fontType,
-    setFontType,
-    imageOpacity,
-    setImageOpacity,
   } = useInput();
 
   return (
     <SubToolbarContext.Provider
       value={{
-        subToolbarType,
-        setSubToolbarType,
-        isBold,
-        setIsBold,
-        isItalic,
-        setIsItalic,
-        isUnderLine,
-        setIsUnderLine,
-        textAlign,
-        setTextAlign,
-        colorValue,
-        setColorValue,
-        buttonColor,
-        setButtonColor,
         localImageSrc,
         setLocalImageSrc,
         clearCanvasBackgroundColor,
         setClearCanavasBackgroundColor,
-        isCanvasClear,
-        setIsCanvasClear,
         imageUrl,
         setImageUrl,
         hasImageUrl,
         setHasImageUrl,
-        imageBrightness,
-        setImageBrightness,
-        imageBlur,
-        setImageBlur,
       }}
     >
       <InputFieldContext.Provider
@@ -88,12 +52,6 @@ const SubToolbarTypeProvider = ({ children }) => {
           setButtonRadius,
           buttonOpacity,
           setButtonOpacity,
-          fontSize,
-          setFontSize,
-          fontType,
-          setFontType,
-          imageOpacity,
-          setImageOpacity,
         }}
       >
         {children}
