4f6409b3c9a7fdd54275cf5bbeaf142cac05c4e2
rami0617 <aarmakrr@hanmail.net>
2022-07-30 11:40:31 +0900

    [TEST]: Add BackgroundChoice component test

--- src/components/BackgroundChoice.test.js
+++ src/components/BackgroundChoice.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 
 import BackgroundChoice from "./BackgroundChoice";
@@ -7,4 +7,10 @@ it("1. Displays button choice header and button correctly", () => {
   render(<BackgroundChoice />);
 
   expect(screen.getByText("Add Background")).toBeInTheDocument();
+});
+
+it("2. Displays input color in BackgroundChoice component", () => {
+  const { container } = render(<BackgroundChoice />);
+
+  expect(container.getElementsByClassName("colorPalette")).toBeVisible;
 });
--- src/components/ImageBrightnessSubToolbar.test.js
+++ src/components/ImageBrightnessSubToolbar.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 
 import { SubToolbarTypeProvider } from "../context/subToolbarContext";
