b6972cfceb4d0444314d5828558ec0fbdcfdec60
unjinjang <alpsboy1996@gmail.com>
2022-07-31 05:35:48 +0900

    [REFACTOR]: Change logic handling user login status

--- src/App.js
+++ src/App.js
@@ -7,22 +7,29 @@ import ErrorSection from "./components/ErrorSection";
 import Loader from "./components/Loader";
 import Login from "./components/Login";
 import UserWebsites from "./components/UserWebsites";
-import { UserContext } from "./context/userContext";
-import { Store } from "./store/Store";
+import { Context } from "./store/Store";
 
 function App() {
-  const { isLoggedIn } = useContext(UserContext);
+  const { globalState } = useContext(Context);
+  const { loggedInUserInformation } = globalState;
 
   return (
-    <Store>
-      <Routes>
-        <Route path="/" element={isLoggedIn ? <UserWebsites /> : <Login />} />
-        <Route path="/editor/:id" element={<Editor />} />
-        <Route path="/creatingnewwebsite" element={<Loader />} />
-        <Route path="/userwebsite/:id/deployed" element={<DeployedWebsite />} />
-        <Route path="/*" element={<ErrorSection />} />
-      </Routes>
-    </Store>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          loggedInUserInformation.tokenInformation ? (
+            <UserWebsites />
+          ) : (
+            <Login />
+          )
+        }
+      />
+      <Route path="/editor/:id" element={<Editor />} />
+      <Route path="/creatingnewwebsite" element={<Loader />} />
+      <Route path="/userwebsite/:id/deployed" element={<DeployedWebsite />} />
+      <Route path="/*" element={<ErrorSection />} />
+    </Routes>
   );
 }
 
