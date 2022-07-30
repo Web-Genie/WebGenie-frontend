a41d8a253e206f751509d15d0ef2474313e30566
rami0617 <aarmakrr@hanmail.net>
2022-07-30 11:05:49 +0900

    [TEST]: Add test code in Modal component

--- src/components/Modal.test.js
+++ src/components/Modal.test.js
@@ -30,3 +30,30 @@ it("1. show modal when modal component execution and text modal content", () => 
   expect(modal).toBeTruthy();
   expect(screen.getByText("hello")).toContainHTML("div");
 });
+
+it("2. should have style in modal div", () => {
+  let portalRoot = document.getElementById("modal-root");
+
+  if (!portalRoot) {
+    portalRoot = document.createElement("div");
+    portalRoot.setAttribute("id", "modal-root");
+    portalRoot.setAttribute("test-id", "modal-root");
+    document.body.appendChild(portalRoot);
+  }
+
+  render(
+    <ModalPortal>
+      <Modal>
+        <div className="hello">hello</div>
+      </Modal>
+    </ModalPortal>
+  );
+
+  screen.debug();
+
+  const div = screen.getByText("hello");
+
+  expect(div).toHaveStyle(`display: flex`);
+  expect(div).toHaveStyle(`margin: 10px 20px`);
+  expect(div).toHaveStyle(`flex-direction: column`);
+});
