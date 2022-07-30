8ea0070007608cbd735b544d44329115a44f5b9c
unjinjang <alpsboy1996@gmail.com>
2022-07-31 05:42:46 +0900

    [REFACTOR]: Add extra logic to handle edge case

--- src/utils/transformRgbToHex.js
+++ src/utils/transformRgbToHex.js
@@ -1,4 +1,16 @@
-const transformRgbToHex = (red, green, blue) =>
-  "#" + red.toString(16) + green.toString(16) + blue.toString(16);
+const transformRgbToHex = (red, green, blue) => {
+  let hexColor =
+    "#" + red.toString(16) + green.toString(16) + blue.toString(16);
+
+  if (hexColor.length < 7) {
+    const difference = 7 - hexColor.length;
+
+    for (let i = 0; i < difference; i++) {
+      hexColor += "0";
+    }
+  }
+
+  return hexColor;
+};
 
 export default transformRgbToHex;
