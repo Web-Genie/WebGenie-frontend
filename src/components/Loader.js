1a1a2a01f2ffaab0a7b1997a205dfdb0cee6c653
unjinjang <alpsboy1996@gmail.com>
2022-08-01 22:47:23 +0900

    [FEAT]: Add logic to automatically send user to homepage when loading takes more than 8 seconds

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DotLoader from "react-spinners/DotLoader";
 import styled from "styled-components";
 
+import useLogout from "../hooks/useLogout";
 function Loader() {
+  const [, setCurrentTimer] = useState(0);
+  const { handleLogout } = useLogout();
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTimer((prev) => {
+        if (prev === 10) {
+          handleLogout();
+        }
+
+        return (prev += 1);
+      });
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
     <LoaderContainer>
       <DotLoader color="#023E8A" size={60} />
