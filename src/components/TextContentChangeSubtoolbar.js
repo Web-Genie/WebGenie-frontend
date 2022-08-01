262596795a9b72521fc7ddcf3e7cdaaf564bde8d
unjinjang <alpsboy1996@gmail.com>
2022-08-01 21:24:31 +0900

    [FEAT]: Add logic to change draggable elements text content

---
+++ src/components/TextContentChangeSubtoolbar.js
@@ -0,0 +1,56 @@
+import React, { useContext, useState } from "react";
+import styled from "styled-components";
+
+import { Context } from "../store/Store";
+import SubToolbarTitle from "./SubToolbarTitle";
+import ToolbarButton from "./ToolbarButton";
+
+function TextContentChangeSubtoolbar() {
+  const { globalState } = useContext(Context);
+  const { currentElement } = globalState;
+  const [textareaContent, setTextareaContent] = useState(
+    currentElement.innerHTML
+  );
+
+  return (
+    <div className="choiceContainer">
+      <SubToolbarTitle title="Change text content" />
+      <div className="detailContainer" data-testid="detailContainer">
+        <TextareaContainer>
+          <textarea
+            defaultValue={textareaContent.replace(/<br ?\/?>/g, "\n")}
+            onChange={() =>
+              setTextareaContent(event.target.value.replace(/\n/g, "<br/>"))
+            }
+          />
+          <ToolbarButton
+            handleClick={() => (currentElement.innerHTML = textareaContent)}
+          >
+            apply
+          </ToolbarButton>
+        </TextareaContainer>
+      </div>
+    </div>
+  );
+}
+
+const TextareaContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  textarea {
+    width: 100%;
+    height: 50px;
+    padding: 7px;
+  }
+
+  button {
+    margin-top: 10px;
+    width: 50px;
+
+    cursor: pointer;
+  }
+`;
+
+export default TextContentChangeSubtoolbar;
